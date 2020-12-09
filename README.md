c0e3b58f9f8f38f8f714d409b06fc0f6327b6507
Bart Moorman <github@id10t.us>
2020-12-09 11:54:29 -0700

    Add ability to change collection frequency Be more verbose about what's going on

--- README.md
+++ README.md
@@ -29,3 +29,4 @@ services:
 |DASHBOARD_KEY|Sets the Dashboard key for this sensor|`<empty>`|
 |DHT_PIN|Sets the input pin to which the environment sensor is attached|`<empty>`|
 |DHT_TYPE|Sets the attached sensor type (`DHT11`, `DHT22`, or `AM2302`)|`DHT22`|
+|FREQUENCY|Sets how often to collect (and send) readings [seconds]|`30`|
--- bin/sensor.py
+++ bin/sensor.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python
-import os, requests
+import sys, os, requests
 from time import sleep
 from datetime import datetime
 import Adafruit_DHT as DHT
 
-DHT_PIN = int(os.getenv('DHT_PIN'))
+DHT_PIN = os.getenv('DHT_PIN')
 DHT_TYPE = getattr(DHT, os.getenv('DHT_TYPE', 'DHT22'))
 DASHBOARD_HOST = os.getenv('DASHBOARD_HOST')
 DASHBOARD_KEY = os.getenv('DASHBOARD_KEY')
+FREQUENCY = os.getenv('FREQUENCY', 30)
 SESSION = requests.Session()
 
+print '''Starting Sensor with the following settings:
+DHT_PIN: {0}
+DHT_TYPE: {1}
+DASHBOARD_HOST: {2}
+DASHBOARD_KEY: {3}
+FREQUENCY: {4}
+'''.format(DHT_PIN or '[unset]', DHT_TYPE, DASHBOARD_HOST or '[unset]', '[set]' if DASHBOARD_KEY else '[unset]', FREQUENCY)
+
+if DASHBOARD_HOST is None or DASHBOARD_KEY is None:
+  print('Dashboard is not configured. Readings will be sent to stdout.')
+
 while True:
-  humidity, temperature = DHT.read_retry(DHT_TYPE, DHT_PIN)
-  if humidity is not None and temperature is not None:
-    if DASHBOARD_HOST is not None and DASHBOARD_KEY is not None:
-      payload = {'func': 'putReading', 'key': DASHBOARD_KEY, 'temperature': temperature, 'humidity': humidity}
-      try:
-        SESSION.post(DASHBOARD_HOST + '/src/action.php', data=payload, timeout=5.0)
-      except requests.exceptions.RequestException as exception:
-        print('{0} - {1}'.format(datetime.now(), exception))
+  if DHT_PIN is not None:
+    humidity, temperature = DHT.read_retry(DHT_TYPE, int(DHT_PIN))
+    if humidity is not None and temperature is not None:
+      if DASHBOARD_HOST is not None and DASHBOARD_KEY is not None:
+        payload = {'func': 'putReading', 'key': DASHBOARD_KEY, 'temperature': temperature, 'humidity': humidity}
+        try:
+          SESSION.post(DASHBOARD_HOST + '/src/action.php', data=payload, timeout=5.0)
+        except requests.exceptions.RequestException as exception:
+          print('{0} - {1}'.format(datetime.now(), exception))
+      else:
+        print('{0} - Temperature={1:0.2f}*C, Humidity={2:0.2f}%'.format(datetime.now(), temperature, humidity))
+      sleep(FREQUENCY)
     else:
-      print('{0} - Dasboard host is not configured'.format(datetime.now()))
-      print('Temperature={0:0.2f}*C, Humidity={1:0.2f}%'.format(temperature, humidity))
-    sleep(30)
+      print('{0} - Unable to read {1} on GPIO {2}'.format(datetime.now(), DHT_TYPE, DHT_PIN))
+      sleep(5)
   else:
-    print('{0} - Unable to read {1} on GPIO {2}'.format(datetime.now(), DHT_TYPE, DHT_PIN))
-    sleep(5)
+    print('DHT_PIN is required.')
+    sleep(60)
